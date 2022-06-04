e342ec283383fe1dfaf6d0ddb94a06a9f3d9c5cb
Andrey Zavrichko <andrey@zavrichko.dev>
2022-06-04 10:17:24 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+    $('.photo__slider').slick({        
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg"></button>'
+    });
+
+
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+
+});
